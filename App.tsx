2c7dc1ffa97f35b5f8a46e45ae2c8e6e65129b7c
eexy <eexdhe@gmail.com>
2024-12-15 17:11:59 +0100

    feat: add and configure sqlite db with drizzle orm

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import {drizzle} from 'drizzle-orm/expo-sqlite';
+import {useMigrations} from "drizzle-orm/expo-sqlite/migrator";
+import migrations from "./drizzle/migrations";
+
+const expo = SQLite.openDatabaseSync('workouts.db');
+
+const db = drizzle(expo);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const {success, error} = useMigrations(db, migrations)
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
