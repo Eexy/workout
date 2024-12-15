90df1172b0d2926cbeeb47a6a224dd80c7d7c012
eexy <eexdhe@gmail.com>
2024-12-15 18:02:40 +0100

    chore: create db migrations and add missing package babel-plugin-inline-import

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,13 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_icy_white_queen.sql';
+import m0001 from './0001_late_leper_queen.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001
     }
   }
   
